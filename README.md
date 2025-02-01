6834b1a1e0bd892dbc7c3e38107c7027aa98cf45
Lance Bassett <lanbas@umich.edu>
2025-02-01 11:02:25 -0500

    Initial style commit, v0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # pushup-bank
+beefy boy bank branch
+
+### Contributing 
+Initial development has been developed and tested on Ubuntu 22.04 and Python 3.10.12
+
+#### Setup Environment
+
+```
+git clone git@github.com:lanbas/pushup-bank.git
+cd pushup-bank
+```
+
+#### Setup virtual environment
+```
+python3 -m venv env
+source env/bin/activate
+```
+
+
+#### Install dependencies via ```pip```
+```
+(env) python3 -m pip install -r requirements.txt
+```
+
+### Test Installation
+Once the dependencies have been installed, the follow command should run the server: 
+```
+(env) fastapi dev server.py
+```
+
+The server will be accessible on http://localhost:8000/home.
+
+### Accessing from a mobile device
+Run the server under production settings: 
+```
+(env) fastapi run server.py
+```
+
+The server will be accessible from another device on http://<host computer's IP>:8000/home
--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+const DAYS_PER_MONTH = {
+    0: 0,
+    1: 31,
+    2: 59,
+    3: 90,
+    4: 120,
+    5: 151,
+    6: 181,
+    7: 212,
+    8: 243,
+    9: 273,
+    10: 304,
+    11: 334
+}
+
+function calcDailyPushups()
+{
+    // Get date information
+    let date = new Date();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let year = date.getFullYear();
+    console.log(day);
+
+    // Calculate based on months passed + days passed this month
+    let numPushups = DAYS_PER_MONTH[month] + day;
+
+    // Check for leap year
+    // If divisible by 4 and not a century year, it's a leap year. If it's a century year, only century years divisible by 400 are leap years. 
+    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+    {
+        numPushups += 1;
+    }
+
+    return numPushups;
+}
 
 if(document.readyState !== 'loading') 
 {
@@ -10,10 +46,33 @@ else
     })
 }
 
+function placeDailyPushupElement(numPushups)
+{
+    let pushupDiv = document.createElement("div");
+    pushupDiv.id = "pushupDiv";
+
+    let pushupElt = document.createElement("h2");
+    pushupElt.innerText = numPushups;
+    pushupElt.id = "pushupElt"
+
+    let pushupMsg = document.createElement("p");
+    pushupMsg.innerText = "push-ups due today";
+    pushupMsg.id = "pushupMsg";
+
+    let title = document.getElementById("title");
+    title.insertAdjacentElement('afterend', pushupDiv);
+    pushupDiv.appendChild(pushupElt);
+    pushupElt.insertAdjacentElement('afterend', pushupMsg);
+}
+
 function initialize()
 {
     // Fetch user data and display
     getBankAmounts();
+
+    // Calculate number of push-ups required today and display
+    let numPushups = calcDailyPushups();
+    placeDailyPushupElement(numPushups);
 
     // Add event listener for form submission
     let additionForm = document.getElementById("additionForm");
